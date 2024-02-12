c79b932ac6392fac2c5be6cfc383babb4db89f27
connordilgren <connord0127@gmail.com>
2024-02-11 23:23:42 -0600

    load random

--- UI.py
+++ UI.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pickle
+import random
 
 import PySimpleGUI as sg
 
@@ -106,19 +107,19 @@ def validate_input(values):
             'rooted': 59,
             'missing': 60,
         },
-        'stalk-surface above ring': {
+        'stalk-surface-above-ring': {
             'fibrous': 61,
             'scaly': 62,
             'silky': 63,
             'smooth': 64,
         },
-        'stalk-surface below ring': {
+        'stalk-surface-below-ring': {
             'fibrous': 65,
             'scaly': 66,
             'silky': 67,
             'smooth': 68,
         },
-        'stalk-color above ring': {
+        'stalk-color-above-ring': {
             'brown': 69,
             'buff': 70,
             'cinnamon': 71,
@@ -206,7 +207,7 @@ def validate_input(values):
     return 0
 
 
-def give_results(prediction):
+def give_results(prediction, df_input):
     if prediction == 0:
         result = 'edible'
     else:
@@ -217,6 +218,31 @@ def give_results(prediction):
         [sg.Text(f"This mushroom is {result}.")],
         [sg.Text("Warning: This result may be incorrect. Always consult an "
                  "expert before consuming unidentified mushrooms.")],
+        [sg.Text("Your inputs:")],
+
+        [sg.Text(f"cap-shape: {to_feat('cap-shape', df_input.loc[0, 'cap-shape'])}")],
+        [sg.Text(f"cap-surface: {to_feat('cap-surface', df_input.loc[0, 'cap-surface'])}")],
+        [sg.Text(f"cap-color: {to_feat('cap-color', df_input.loc[0, 'cap-color'])}")],
+        [sg.Text(f"bruises: {to_feat('bruises', df_input.loc[0, 'bruises'])}")],
+        [sg.Text(f"odor: {to_feat('odor', df_input.loc[0, 'odor'])}")],
+        [sg.Text(f"gill-attachment: {to_feat('gill-attachment', df_input.loc[0, 'gill-attachment'])}")],
+        [sg.Text(f"gill-spacing: {to_feat('gill-spacing', df_input.loc[0, 'gill-spacing'])}")],
+        [sg.Text(f"gill-size: {to_feat('gill-size', df_input.loc[0, 'gill-size'])}")],
+        [sg.Text(f"gill-color: {to_feat('gill-color', df_input.loc[0, 'gill-color'])}")],
+        [sg.Text(f"stalk-shape: {to_feat('stalk-shape', df_input.loc[0, 'stalk-shape'])}")],
+        [sg.Text(f"stalk-root: {to_feat('stalk-root', df_input.loc[0, 'stalk-root'])}")],
+        [sg.Text(f"stalk-surface-above-ring: {to_feat('stalk-surface-above-ring', df_input.loc[0, 'stalk-surface-above-ring'])}")],
+        [sg.Text(f"stalk-surface-below-ring: {to_feat('stalk-surface-below-ring', df_input.loc[0, 'stalk-surface-below-ring'])}")],
+        [sg.Text(f"stalk-color-above-ring: {to_feat('stalk-color-above-ring', df_input.loc[0, 'stalk-color-above-ring'])}")],
+        [sg.Text(f"stalk-color-below-ring: {to_feat('stalk-color-below-ring', df_input.loc[0, 'stalk-color-below-ring'])}")],
+        [sg.Text(f"veil-type: {to_feat('veil-type', df_input.loc[0, 'veil-type'])}")],
+        [sg.Text(f"veil-color: {to_feat('veil-color', df_input.loc[0, 'veil-color'])}")],
+        [sg.Text(f"ring-number: {to_feat('ring-number', df_input.loc[0, 'ring-number'])}")],
+        [sg.Text(f"ring-type: {to_feat('ring-type', df_input.loc[0, 'ring-type'])}")],
+        [sg.Text(f"spore-print-color: {to_feat('spore-print-color', df_input.loc[0, 'spore-print-color'])}")],
+        [sg.Text(f"population: {to_feat('population', df_input.loc[0, 'population'])}")],
+        [sg.Text(f"habitat: {to_feat('habitat', df_input.loc[0, 'habitat'])}")],
+
         [sg.Button("Exit")]
         ]
     window_resuls = sg.Window("Results", layout, modal=True)
@@ -226,6 +252,185 @@ def give_results(prediction):
             break
 
     window_resuls.close()
+
+
+def to_feat(field, num):
+    fields = {
+        'cap-shape': {
+            'bell': 0,
+            'conical': 1,
+            'convex': 2,
+            'flat': 3,
+            'knobbed': 4,
+            'sunken': 5,
+        },
+        'cap-surface': {
+            'fibrous': 6,
+            'grooves': 7,
+            'scaly': 8,
+            'smooth': 9,
+        },
+        'cap-color': {
+            'brown': 10,
+            'buff': 11,
+            'cinnamon': 12,
+            'gray': 13,
+            'green': 14,
+            'pink': 15,
+            'purple': 16,
+            'red': 17,
+            'white': 18,
+            'yellow': 19,
+        },
+        'bruises': {
+            'Yes': 20,
+            'No': 21,
+        },
+        'odor': {
+            'almond': 22,
+            'anise': 23,
+            'creosote': 24,
+            'fishy': 25,
+            'foul': 26,
+            'musty': 27,
+            'none': 28,
+            'pungent': 29,
+            'spicy': 30,
+        },
+        'gill-attachment': {
+            'attached': 31,
+            'descending': 32,
+            'free': 33,
+            'notched': 34,
+        },
+        'gill-spacing': {
+            'close': 35,
+            'crowded': 36,
+            'distant': 37,
+        },
+        'gill-size': {
+            'broad': 38,
+            'narrow': 39,
+        },
+        'gill-color': {
+            'black': 40,
+            'brown': 41,
+            'buff': 42,
+            'chocolate': 43,
+            'gray': 44,
+            'green': 45,
+            'orange': 46,
+            'pink': 47,
+            'purple': 48,
+            'red': 49,
+            'white': 50,
+            'yellow': 51,
+        },
+        'stalk-shape': {
+            'enlarging': 52,
+            'tapering': 53,
+        },
+        'stalk-root': {
+            'bulbous': 54,
+            'club': 55,
+            'cup': 56,
+            'equal': 57,
+            'rhizomorphs': 58,
+            'rooted': 59,
+            'missing': 60,
+        },
+        'stalk-surface-above-ring': {
+            'fibrous': 61,
+            'scaly': 62,
+            'silky': 63,
+            'smooth': 64,
+        },
+        'stalk-surface-below-ring': {
+            'fibrous': 65,
+            'scaly': 66,
+            'silky': 67,
+            'smooth': 68,
+        },
+        'stalk-color-above-ring': {
+            'brown': 69,
+            'buff': 70,
+            'cinnamon': 71,
+            'gray': 72,
+            'orange': 73,
+            'pink': 74,
+            'red': 75,
+            'white': 76,
+            'yellow': 77,
+        },
+        'stalk-color-below-ring': {
+            'brown': 78,
+            'buff': 79,
+            'cinnamon': 80,
+            'gray': 81,
+            'orange': 82,
+            'pink': 83,
+            'red': 84,
+            'white': 85,
+            'yellow': 86,
+        },
+        'veil-type': {
+            'partial': 87,
+            'universal': 88,
+        },
+        'veil-color': {
+            'brown': 89,
+            'orange': 90,
+            'white': 91,
+            'yellow': 92,
+        },
+        'ring-number': {
+            'none': 93,
+            'one': 94,
+            'two': 95,
+        },
+        'ring-type': {
+            'cobwebby': 96,
+            'evanescent': 97,
+            'flaring': 98,
+            'large': 99,
+            'none': 100,
+            'pendant': 101,
+            'sheathing': 102,
+            'zone': 103,
+        },
+        'spore-print-color': {
+            'black': 104,
+            'brown': 105,
+            'buff': 106,
+            'chocolate': 107,
+            'green': 108,
+            'orange': 109,
+            'purple': 110,
+            'white': 111,
+            'yellow': 112,
+        },
+        'population': {
+            'abundant': 113,
+            'clustered': 114,
+            'numerous': 115,
+            'scattered': 116,
+            'several': 117,
+            'solitary': 118,
+        },
+        'habitat': {
+            'grasses': 119,
+            'leaves': 120,
+            'meadows': 121,
+            'paths': 122,
+            'urban': 123,
+            'waste': 124,
+            'woods': 125,
+        }
+    }
+
+    for k, v in fields[field].items():
+        if v == num:
+            return k
 
 
 def format_input(values):
@@ -574,7 +779,8 @@ def on_submit(values, model):
     else:
         df_input = format_input(values)
         prediction = model.predict(df_input)
-        give_results(prediction)
+        give_results(prediction, df_input)
+        clear_input(window_main, values)
 
 
 def warning_clear():
@@ -600,16 +806,227 @@ def warning_clear():
 
 
 def clear_input(window_main, values):
+    for i in range(126):
+        window_main[i].reset_group()
+
+
+def clear_input_helper(window_main, values):
     # check if all fields are entered
     if validate_input(values) == 0:
         response = warning_clear()
         if response == "Yes":
-            for i in range(126):
-                window_main[i].reset_group()
+            clear_input(window_main, values)
     else:
-        for i in range(126):
-            window_main[i].reset_group()
+        clear_input(window_main, values)
 
+
+def load_random(window_main, values):
+    # set random values
+    fields = {
+        'cap-shape': {
+            'bell': 0,
+            'conical': 1,
+            'convex': 2,
+            'flat': 3,
+            'knobbed': 4,
+            'sunken': 5,
+        },
+        'cap-surface': {
+            'fibrous': 6,
+            'grooves': 7,
+            'scaly': 8,
+            'smooth': 9,
+        },
+        'cap-color': {
+            'brown': 10,
+            'buff': 11,
+            'cinnamon': 12,
+            'gray': 13,
+            'green': 14,
+            'pink': 15,
+            'purple': 16,
+            'red': 17,
+            'white': 18,
+            'yellow': 19,
+        },
+        'bruises': {
+            'Yes': 20,
+            'No': 21,
+        },
+        'odor': {
+            'almond': 22,
+            'anise': 23,
+            'creosote': 24,
+            'fishy': 25,
+            'foul': 26,
+            'musty': 27,
+            'none': 28,
+            'pungent': 29,
+            'spicy': 30,
+        },
+        'gill-attachment': {
+            'attached': 31,
+            'descending': 32,
+            'free': 33,
+            'notched': 34,
+        },
+        'gill-spacing': {
+            'close': 35,
+            'crowded': 36,
+            'distant': 37,
+        },
+        'gill-size': {
+            'broad': 38,
+            'narrow': 39,
+        },
+        'gill-color': {
+            'black': 40,
+            'brown': 41,
+            'buff': 42,
+            'chocolate': 43,
+            'gray': 44,
+            'green': 45,
+            'orange': 46,
+            'pink': 47,
+            'purple': 48,
+            'red': 49,
+            'white': 50,
+            'yellow': 51,
+        },
+        'stalk-shape': {
+            'enlarging': 52,
+            'tapering': 53,
+        },
+        'stalk-root': {
+            'bulbous': 54,
+            'club': 55,
+            'cup': 56,
+            'equal': 57,
+            'rhizomorphs': 58,
+            'rooted': 59,
+            'missing': 60,
+        },
+        'stalk-surface-above-ring': {
+            'fibrous': 61,
+            'scaly': 62,
+            'silky': 63,
+            'smooth': 64,
+        },
+        'stalk-surface-below-ring': {
+            'fibrous': 65,
+            'scaly': 66,
+            'silky': 67,
+            'smooth': 68,
+        },
+        'stalk-color-above-ring': {
+            'brown': 69,
+            'buff': 70,
+            'cinnamon': 71,
+            'gray': 72,
+            'orange': 73,
+            'pink': 74,
+            'red': 75,
+            'white': 76,
+            'yellow': 77,
+        },
+        'stalk-color-below-ring': {
+            'brown': 78,
+            'buff': 79,
+            'cinnamon': 80,
+            'gray': 81,
+            'orange': 82,
+            'pink': 83,
+            'red': 84,
+            'white': 85,
+            'yellow': 86,
+        },
+        'veil-type': {
+            'partial': 87,
+            'universal': 88,
+        },
+        'veil-color': {
+            'brown': 89,
+            'orange': 90,
+            'white': 91,
+            'yellow': 92,
+        },
+        'ring-number': {
+            'none': 93,
+            'one': 94,
+            'two': 95,
+        },
+        'ring-type': {
+            'cobwebby': 96,
+            'evanescent': 97,
+            'flaring': 98,
+            'large': 99,
+            'none': 100,
+            'pendant': 101,
+            'sheathing': 102,
+            'zone': 103,
+        },
+        'spore-print-color': {
+            'black': 104,
+            'brown': 105,
+            'buff': 106,
+            'chocolate': 107,
+            'green': 108,
+            'orange': 109,
+            'purple': 110,
+            'white': 111,
+            'yellow': 112,
+        },
+        'population': {
+            'abundant': 113,
+            'clustered': 114,
+            'numerous': 115,
+            'scattered': 116,
+            'several': 117,
+            'solitary': 118,
+        },
+        'habitat': {
+            'grasses': 119,
+            'leaves': 120,
+            'meadows': 121,
+            'paths': 122,
+            'urban': 123,
+            'waste': 124,
+            'woods': 125,
+        }
+    }
+
+    df_input = pd.DataFrame({
+                                'cap-shape': [None],
+                                'cap-surface': [None],
+                                'cap-color': [None],
+                                'bruises': [None],
+                                'odor': [None],
+                                'gill-attachment': [None],
+                                'gill-spacing': [None],
+                                'gill-size': [None],
+                                'gill-color': [None],
+                                'stalk-shape': [None],
+                                'stalk-root': [None],
+                                'stalk-surface-above-ring': [None],
+                                'stalk-surface-below-ring': [None],
+                                'stalk-color-above-ring': [None],
+                                'stalk-color-below-ring': [None],
+                                'veil-type': [None],
+                                'veil-color': [None],
+                                'ring-number': [None],
+                                'ring-type': [None],
+                                'spore-print-color': [None],
+                                'population': [None],
+                                'habitat': [None]
+                                })
+
+    for field in fields:
+        k = random.choice([v for v in fields[field].values()])
+        df_input.loc[0, field] = k
+
+    prediction = model.predict(df_input)
+    give_results(prediction, df_input)
+    clear_input(window_main, values)
 
 col_1 = [
     [sg.Text("1. What is the mushroom's cap shape?")],
@@ -735,7 +1152,7 @@ col_1 = [
 
     [sg.Text("14. What is the mushroom's stalk color, above the ring?")],
     [
-        sg.Radio('brown', 'stalk color above ring'),
+        sg.Radio('brown', 'stalk-color-above-ring'),
         sg.Radio('buff', 'stalk-color-above-ring'),
         sg.Radio('cinnamon', 'stalk-color-above-ring'),
         sg.Radio('gray', 'stalk-color-above-ring'),
@@ -828,7 +1245,7 @@ col_1 = [
 ]
 
 col_2 = [
-    # [sg.Button("Load Random")],
+    [sg.Button("Load Random")],
     [sg.Button("Clear Input")],
     [sg.Button("Submit")],
 ]
@@ -856,8 +1273,10 @@ while True:
     # End program if user closes window_main
     if event == sg.WIN_CLOSED:
         break
+    elif event == "Load Random":
+        load_random(window_main, values)
     elif event == "Clear Input":
-        clear_input(window_main, values)
+        clear_input_helper(window_main, values)
     elif event == "Submit":
         on_submit(values, model)
 
